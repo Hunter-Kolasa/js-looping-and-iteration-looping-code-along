307632c94391387d0c3cfa5ee1b58b6e049b2035
Hunter <kolasa.hunter@aol.com>
2021-02-17 04:24:39 -0500

    commit tree 2961d8cc140bb6b2e75b21361f1128323921b055

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
-// Code your solutions in this file
+function writeCards(namesArray, occasion) {
+    let cards = [];
+    for (let i = 0; i < namesArray.length; i++) {
+        cards.push(`Thank you, ${namesArray[i]}, for the wonderful ${occasion} gift!`);
+    } 
+    return cards
+}
+
+function countDown( countdownNumber) {
+    while (countdownNumber > 0) {
+        console.log(countdownNumber);
+        countdownNumber -= 1;
+    }
+    console.log(countdownNumber)
+}
